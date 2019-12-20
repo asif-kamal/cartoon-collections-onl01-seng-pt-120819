534bd3bf14e4226952c75bb56c609f696c619ef8
Asif Kamal <asif_kamal@hotmail.com>
2019-12-20 19:30:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-   array.include(cheese_types)
+   cheese_types = ["cheddar", "gouda", "camembert"]
+   new_array = []
+   array.include?(cheese_types)
+   new_array <<
+   
 end
